aea3165d56fc757d9bfed9d39ae5b4f6d55993c6
Sergey Kharlamov <ch3rryhq@Sergeys-MacBook-Air.local>
2019-05-12 00:43:35 +0300

    born2code : faqin fuckin

--- bots_n_bolts/born2codebot_dir/main.py
+++ bots_n_bolts/born2codebot_dir/main.py
@@ -3,11 +3,15 @@
 # Built with grace on python-telegram-bot
 import logging as log
 import os
+from uuid import uuid4
 
-from telegram import ReplyKeyboardMarkup, CallbackQuery
-from telegram.ext import (Updater, CallbackContext, ConversationHandler, Filters)
+from telegram import (ReplyKeyboardMarkup, CallbackQuery, InlineQueryResultArticle, ParseMode, 
+    						InputTextMessageContent)
+from telegram.ext import (Updater, CallbackContext, ConversationHandler,
+							InlineQueryHandler, Filters)
 from telegram.ext import CommandHandler as CMH 
 from telegram.ext import MessageHandler as MSH
+from telegram.utils.helpers import escape_markdown
 
 ####### SETTINGS #######
 #~~~~~~~ Logging ~~~~~~#
@@ -49,9 +53,11 @@ def	to_story(update, context):
 
 
 def	to_faq(update, context):
-	update.message.reply_text("Загляни чуть позже;)\n"
-								"Ведутся работы!",
-								reply_markup=markup)
+	update.message.reply_text("Здесь, ты можешь узнать больше о нашей школе\n"
+								"Выбери вопрос в выпадающем списке!\n"
+								"Используй руки для навигации ;)\n"
+								"Обработка естественных запросов coming soon:)",
+								reply_markup=faq_markup)
 	return FAQ
 
 
@@ -90,6 +96,31 @@ def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
+def inlinequery(update, context):
+    """Handle the inline query."""
+    query = update.inline_query.query
+    results = [
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Caps",
+            input_message_content=InputTextMessageContent(
+                query.upper())),
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Bold",
+            input_message_content=InputTextMessageContent(
+                "*{}*".format(escape_markdown(query)),
+                parse_mode=ParseMode.MARKDOWN)),
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Italic",
+            input_message_content=InputTextMessageContent(
+                "_{}_".format(escape_markdown(query)),
+                parse_mode=ParseMode.MARKDOWN))]
+
+    update.inline_query.answer(results)
+
+
 ####### IGNITION #######
 TOKEN = os.getenv("BOT_API_TOKEN")
 bot_core = Updater(TOKEN, use_context=True)
@@ -97,15 +128,36 @@ bot = bot_core.dispatcher
 
 #======= LOGICS =======#
 
-#˜˜˜˜˜˜  KEYMAP  ˜˜˜˜˜˜#
-reply_keyboard = [['🌈 Узнать больше о Школе 🌈'],
-				  ['🤷‍♂️ FAQ 🤷', '🔮 Всякое 🔮'],
+#˜˜˜˜˜˜ KEYMAPS  ˜˜˜˜˜˜#
+
+# MAIN MARKUP
+main_keyboard = [['🌈 Подписаться на 21_Daily_Tips 🌈'],
+				  ['🤷‍♂️ 21_FAQ 🤷', '🔮 Всякое 🔮'],
                   ['📲 Свяжись с нами! 📲'],
 				  ['^🕸🔗📱$']]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+markup = ReplyKeyboardMarkup(main_keyboard, one_time_keyboard=True)
+
+# FAQ MARKUP
+faq_keyboard = [['👈🏿, 🤙🏿'],
+				  ['🤷‍♂️ FAQ 🤷', '🔮 Всякое 🔮'],
+				  ['🌈 Подписаться на 21_Daily_Tips 🌈']]
+faq_markup = ReplyKeyboardMarkup(faq_keyboard, one_time_keyboard=True)
 
 #˜˜˜˜˜˜  MANAGER ˜˜˜˜˜˜#
 MAIN, STORY, FAQ, MISC, CONTACT = range(5)
+
+# MAIN States
+main_states = [MSH(Filters.regex('^🌈 Подписаться на 21_Daily_Tips 🌈$'), to_story),
+					MSH(Filters.regex('^🤷‍♂️ 21_FAQ 🤷$'), to_faq),
+					MSH(Filters.regex('^🔮 Всякое 🔮$'), to_misc),
+					MSH(Filters.regex('^📲 Свяжись с нами! 📲$'), to_contact, pass_user_data=True),
+					MSH(Filters.regex('^🕸🔗📱$'), to_links)
+]
+
+return_states = [MSH(Filters.regex('^👈🏿$'), rewind),
+					MSH(Filters.regex('^🤙🏿$'), to_links)]
+
+inliner = [InlineQueryHandler(inlinequery)]
 
 conv_handler = ConversationHandler(
 	entry_points=[CMH('start', start),
@@ -113,25 +165,11 @@ conv_handler = ConversationHandler(
 	],
 
 	states={
-		MAIN:	[MSH(Filters.regex('^🌈 Узнать больше о Школе 🌈$'), to_story),
-					MSH(Filters.regex('^🤷‍♂️ FAQ 🤷$'), to_faq),
-					MSH(Filters.regex('^🔮 Всякое 🔮$'), to_misc),
-					MSH(Filters.regex('^📲 Свяжись с нами! 📲$'), to_contact),
-					MSH(Filters.regex('^🕸🔗📱$'), to_links),
-					
-		],
-		STORY:	[
-					
-		],
-		FAQ:	[
-					
-		],
-		MISC:	[
-					
-		],
-		CONTACT:[
-					
-		],
+		MAIN:	main_states,
+		STORY:	main_states.__add__(return_states),
+		FAQ:	main_states.__add__(inliner),
+		MISC:	main_states.__add__(return_states),
+		CONTACT:main_states.__add__(return_states),
 	},
 	fallbacks=[MSH(Filters.all, help)],
 )
